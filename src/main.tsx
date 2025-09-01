1d49855468d84e9e957618ad2310b499865a19a6
amos-babu <amos.babu@yahoo.com>
2025-09-01 13:44:38 +0300

    useInfinityQuery for paginated data

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,41 @@ import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./utils/AuthContext.tsx";
 import { FileContextProvider } from "./utils/FileContext.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { Home } from "./pages/Home.tsx";
+// import { DxfPage } from "./pages/DxfPage.tsx";
+// import { Profile } from "./pages/Profile.tsx";
+// import NotFound from "./pages/NotFound.tsx";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Home />,
+//   },
+//   {
+//     path: "/show/:id",
+//     element: <DxfPage />,
+//   },
+//   {
+//     path: "/profile/:id",
+//     element: <Profile />,
+//   },
+//   {
+//     path: "*",
+//     element: <NotFound />,
+//   },
+// ]);
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <AuthProvider>
-      <FileContextProvider>
-        <App />
-      </FileContextProvider>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <FileContextProvider>
+          <App />
+        </FileContextProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   </BrowserRouter>
 );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,27 +2,38 @@ import { useEffect } from "react";
 import MasonryLayout from "../components/MasonryLayout";
 import { useFile } from "../utils/FileContext";
 import { useInView } from "react-intersection-observer";
+import { useFetchFiles, useFetchPaginatedFiles } from "../hooks/fetchFile";
 
 export const Home = () => {
-  const { files, fetchFiles, hasMore } = useFile();
+  // const { files, fetchFiles, hasMore } = useFile();
   const { ref, inView } = useInView();
 
-  useEffect(() => {
-    fetchFiles();
-  }, []);
+  // useEffect(() => {
+  //   fetchFiles();
+  // }, []);
+
+  // useEffect(() => {
+  //   if (inView) {
+  //     fetchFiles();
+  //   }
+  // }, [inView]);
+
+  const { data: files2, fetchNextPage, hasNextPage } = useFetchPaginatedFiles();
 
   useEffect(() => {
     if (inView) {
-      fetchFiles();
+      fetchNextPage();
     }
   }, [inView]);
-  
 
+  // console.log(status, error, isFetching, isFetchingNextPage);
   return (
     <>
       <div className="row">
-        <MasonryLayout files={files} />
-        {hasMore && (
+        {files2?.pages.flatMap((page) => (
+          <MasonryLayout files={page.data.files} />
+        ))}
+        {hasNextPage && (
           <div ref={ref} className="text-center mt-4">
             <span className="spinner-border"></span>
           </div>
